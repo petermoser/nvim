d38234c4ad49659aaaeef74f744211e76b6d01d0
petermoser <peter.moser@gmail.com>
2023-11-10 10:13:50 +0100

    Fixed some errors

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,26 +1,24 @@
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 -- insert new line with yanked content
-vim.keymap.set('n', 'Q', 'o<Esc>p', { noremap = true })
+vim.keymap.set("n", "Q", "o<Esc>p", { noremap = true })
 -- This maps 'cp' in visual mode to copy the selection to the system clipboard
-vim.api.nvim_set_keymap('v', 'cp', '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "cp", '"+y', { noremap = true, silent = true })
 
-
-vim.keymap.set('i', 'jk', '<Esc>', {noremap = true, silent = true})
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
 -- For insert mode, we create an anonymous function to save and exit insert mode
-vim.keymap.set('i', 'fd', function()
-  vim.cmd('write')
-  -- Return the '<Esc>' key to exit insert mode. We use termcodes to ensure it's treated as a key press.
-  return vim.api.nvim_replace_termcodes('<Esc>', true, true, true)
-end, {expr = true, noremap = true, silent = true})
+vim.keymap.set("i", "fd", function()
+	vim.cmd("write")
+	-- Return the '<Esc>' key to exit insert mode. We use termcodes to ensure it's treated as a key press.
+	return vim.api.nvim_replace_termcodes("<Esc>", true, true, true)
+end, { expr = true, noremap = true, silent = true })
 
 -- For normal mode, it's straightforward since we don't need to exit insert mode
-vim.keymap.set('n', 'fd', ':write<CR>', {noremap = true, silent = true})
+vim.keymap.set("n", "fd", ":write<CR>", { noremap = true, silent = true })
 
 -- This maps the Escape key to switch to Normal mode when you're in a terminal buffer.
-vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], {noremap = true})
-
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { noremap = true })
--- lua/core/plugin_config/gen.lua
+++ lua/core/plugin_config/gen.lua
@@ -1,12 +1,12 @@
 -- remember to start the LLM with for example:
 -- ollama run mistral:instruct
 
-vim.keymap.set('v', '<leader>g', ':Gen<CR>')
-vim.keymap.set('n', '<leader>g', ':Gen<CR>')
+vim.keymap.set("v", "<leader>g", ":Gen<CR>")
+vim.keymap.set("n", "<leader>g", ":Gen<CR>")
 
-require('gen').model = 'mistral:inst1452708639ruct'
+require("gen").model = "mistral:instruct"
 
-require('gen').prompts['Commit Message'] = {
-  prompt = "Create a commit message from the text:\n$text",
-  replace = false
+require("gen").prompts["Commit Message"] = {
+	prompt = "Create a commit message from the text:\n$text",
+	replace = false,
 }
