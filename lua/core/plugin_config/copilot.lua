19195c084ade92d1fac96e9accad118c1a746cbb
petermoser <peter.moser@gmail.com>
2023-12-21 09:36:54 +0100

    Added file types for copilot

--- lua/core/plugin_config/copilot.lua
+++ lua/core/plugin_config/copilot.lua
@@ -1,23 +1,30 @@
 -- copilot settings, those are by default set to false
-require("copilot").setup({
-	suggestion = {
-		auto_trigger = true,
-		debounce = 300,
-		keymap = {
-			-- <Option-y>
-			accept = "¥",
-			-- <Option-f>
-			next = "ƒ",
-			-- <Option-b>
-			prev = "∫",
-			-- <Option-d>
-			dismiss = "∂",
-		},
-	},
-	filetypes = {
-		yaml = true,
-		markdown = true,
-		gitcommit = true,
-		gitrebase = true,
-	},
-})
+-- require("copilot").setup({
+-- 	suggestion = {
+-- 		auto_trigger = true,
+-- 		debounce = 300,
+-- 		keymap = {
+-- 			-- <Option-y>
+-- 			accept = "¥",
+-- 			-- <Option-f>
+-- 			next = "ƒ",
+-- 			-- <Option-b>
+-- 			prev = "∫",
+-- 			-- <Option-d>
+-- 			dismiss = "∂",
+-- 		},
+-- 	},
+-- 	filetypes = {
+-- 		yaml = true,
+-- 		markdown = true,
+-- 		gitcommit = true,
+-- 		gitrebase = true,
+-- 	},
+-- })
+--
+vim.g.copilot_filetypes = {
+	gitcommit = true,
+	gitrebase = true,
+	markdown = true,
+	yaml = true,
+}
