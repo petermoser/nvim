4f1a74eec0fbb8e996aff2729d68fe9980c44578
petermoser <peter.moser@gmail.com>
2023-12-21 09:19:15 +0100

    Added new file types to treesitter

--- lua/core/plugin_config/treesitter.lua
+++ lua/core/plugin_config/treesitter.lua
@@ -1,11 +1,25 @@
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all"
-  ensure_installed = { "c", "lua", "rust", "ruby", "vim" },
+require("nvim-treesitter.configs").setup({
+	-- A list of parser names, or "all"
+	ensure_installed = {
+		"c",
+		"lua",
+		"markdown",
+		"yaml",
+		"vim",
+		"javascript",
+		"typescript",
+		"tsx",
+		"html",
+		"css",
+		"json",
+		"python",
+		"bash",
+	},
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-  auto_install = true,
-  highlight = {
-    enable = true,
-  },
-}
+	-- Install parsers synchronously (only applied to `ensure_installed`)
+	sync_install = false,
+	auto_install = true,
+	highlight = {
+		enable = true,
+	},
+})
