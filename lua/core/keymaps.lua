753a2c19b094a8d20c09542e33db1a628b47694d
petermoser <peter.moser@gmail.com>
2024-03-06 12:52:28 +0100

    Added gc and

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -12,9 +12,8 @@ vim.api.nvim_set_keymap("v", "cp", '"+y', { noremap = true, silent = true })
 vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
 -- For insert mode, we create an anonymous function to save and exit insert mode
 vim.keymap.set("i", "fd", function()
+	vim.cmd("stopinsert")
 	vim.cmd("write")
-	-- Return the '<Esc>' key to exit insert mode. We use termcodes to ensure it's treated as a key press.
-	return vim.api.nvim_replace_termcodes("<Esc>", true, true, true)
 end, { expr = true, noremap = true, silent = true })
 
 -- For normal mode, it's straightforward since we don't need to exit insert mode
@@ -35,9 +34,7 @@ vim.keymap.set("n", "J", "<Nop>", { noremap = true })
 vim.keymap.set("n", "<S-j>", "J", { noremap = true })
 
 -- shortcut for Gwrite and G commit in fugitive:Gwrite
--- :G commit
---
-vim.keymap.set("i", "gcom", function()
+vim.keymap.set("i", "gc", function()
 	-- Exit insert mode first
 	vim.cmd("stopinsert")
 	-- Then write and quit
@@ -51,4 +48,9 @@ vim.keymap.set("i", "wq", function()
 	vim.cmd("stopinsert")
 	-- Then write and quit
 	vim.cmd("wq")
+end, { noremap = true, silent = true })
+
+vim.keymap.set("i", "gw", function()
+	vim.cmd("stopinsert")
+	vim.cmd("Gwrite")
 end, { noremap = true, silent = true })
