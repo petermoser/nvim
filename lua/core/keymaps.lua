0be27fc5ddabad64de1b6bfcbc111ab4e9218c49
petermoser <peter.moser@gmail.com>
2024-03-18 15:22:54 +0100

    Added some fugitive shortcuts

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -34,7 +34,7 @@ vim.keymap.set("n", "J", "<Nop>", { noremap = true })
 vim.keymap.set("n", "<S-j>", "J", { noremap = true })
 
 -- shortcut for Gwrite and G commit in fugitive:Gwrite
-vim.keymap.set("i", "gc", function()
+vim.keymap.set("i", "gco", function()
 	-- Exit insert mode first
 	vim.cmd("stopinsert")
 	-- Then write and quit
@@ -42,15 +42,31 @@ vim.keymap.set("i", "gc", function()
 	vim.cmd("G commit")
 end, { noremap = true, silent = true })
 
--- shortcut for write and quit
+-- shortcut for Gwrite and G commit and push
+vim.keymap.set("n", "gco", function()
+	-- Exit insert mode first
+	vim.cmd("stopinsert")
+	-- Then write and quit
+	vim.cmd("Gwrite")
+	vim.cmd("G commit")
+	vim.cmd("G push")
+end, { noremap = true, silent = true })
+
+-- shortcut for write, quit and git push
 vim.keymap.set("i", "wq", function()
 	-- Exit insert mode first
 	vim.cmd("stopinsert")
 	-- Then write and quit
 	vim.cmd("wq")
+	vim.cmd("G push")
 end, { noremap = true, silent = true })
 
-vim.keymap.set("i", "gw", function()
+vim.keymap.set("i", "gaa", function()
+	vim.cmd("stopinsert")
+	vim.cmd("Gwrite")
+end, { noremap = true, silent = true })
+
+vim.keymap.set("n", "gaa", function()
 	vim.cmd("stopinsert")
 	vim.cmd("Gwrite")
 end, { noremap = true, silent = true })
