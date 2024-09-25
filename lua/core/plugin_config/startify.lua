e262bd6937a8d55860fc201f8aada9bd154e2045
petermoser <peter.moser@gmail.com>
2024-09-25 17:48:25 +0200

    fix: formatting

--- lua/core/plugin_config/startify.lua
+++ lua/core/plugin_config/startify.lua
@@ -7,31 +7,30 @@ vim.g.startify_use_env = 1
 
 -- Custom startup list, only show MRU from current directory/project
 vim.g.startify_lists = {
-  { type = 'dir', header = { 'Files ' .. vim.fn.getcwd() } },
-  { type = 'sessions', header = { 'Sessions' } },
-  { type = 'bookmarks', header = { 'Bookmarks' } },
-  { type = 'commands', header = { 'Commands' } },
+	{ type = "dir", header = { "Files " .. vim.fn.getcwd() } },
+	{ type = "sessions", header = { "Sessions" } },
+	{ type = "bookmarks", header = { "Bookmarks" } },
+	{ type = "commands", header = { "Commands" } },
 }
 
 -- Define startify commands
 vim.g.startify_commands = {
-  { up = { 'Update Plugins', ':PackerSync' } },
+	{ up = { "Update Plugins", ":Lazy" } },
 }
 
 -- Define startify bookmarks
 vim.g.startify_bookmarks = {
-  { c = '~/.config/nvim/init.lua' },
-  { z = '~/.zshrc' },
+	{ c = "~/.config/nvim/init.lua" },
+	{ z = "~/.zshrc" },
 }
 
 -- Auto commands using Lua in Neovim
 vim.api.nvim_create_autocmd("User", {
-  pattern = "Startified",
-  callback = function()
-    vim.wo.cursorline = true
-  end,
+	pattern = "Startified",
+	callback = function()
+		vim.wo.cursorline = true
+	end,
 })
 
 -- Mapping <C-s> to Startify command in Lua
-vim.api.nvim_set_keymap('n', '<C-s>', ':Startify<CR>', { noremap = true, silent = true })
-
+vim.api.nvim_set_keymap("n", "<C-s>", ":Startify<CR>", { noremap = true, silent = true })
