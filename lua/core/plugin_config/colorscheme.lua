ecc4616fafb9701241529c73b5398b296ef12ec2
petermoser <peter.moser@gmail.com>
2024-09-25 15:56:32 +0200

    changed color scheme to tokyo-night

--- lua/core/plugin_config/colorscheme.lua
+++ lua/core/plugin_config/colorscheme.lua
@@ -39,7 +39,7 @@
 -- vim.cmd [[ colorscheme gruvbox]]
 --
 
--- vim.cmd("colorscheme tokyonight-night") -- for night themes
+vim.cmd("colorscheme tokyonight-night") -- for night themes
 -- vim.cmd("colorscheme tokyonight-storm") -- for storm themes
 -- vim.cmd("colorscheme tokyonight-day") -- for day theme
-vim.cmd("colorscheme tokyonight-moon") -- for moon theme
+-- vim.cmd("colorscheme tokyonight-moon") -- for moon theme
