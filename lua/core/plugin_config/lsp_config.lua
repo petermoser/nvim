640704dbcafe90f01f6e8b5fb8dd10bce8383a2c
petermoser <peter.moser@gmail.com>
2024-09-25 17:47:37 +0200

    feat: added more language server

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -1,19 +1,75 @@
+-- Setup mason and mason-lspconfig
 require("mason").setup()
 
 require("mason-lspconfig").setup({
 	ensure_installed = {
-		"lua_ls",
-		"tsserver",
 		"html",
 		"cssls",
+		"lua_ls",
+		"rust_analyzer",
+		"ts_ls",
+		"jsonls",
+		"docker_compose_language_service",
+		"dockerls",
+		"pyright",
+		"tailwindcss",
+		"yamlls",
 	},
 })
 
+-- Import lspconfig
 local lspconfig = require("lspconfig")
 
+-- Set default options for LSP servers
 local lsp_defaults = lspconfig.util.default_config
 
-require("lspconfig").lua_ls.setup({
+-- Define a function to attach completion and keymaps
+local on_attach = function(client, bufnr)
+	-- Enable completion triggered by <c-x><c-o>
+	vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+	-- Buffer local mappings.
+	local opts = { noremap = true, silent = true, buffer = bufnr }
+
+	-- Key mappings
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+	vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+	vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, opts)
+	vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, opts)
+	vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+	vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+	vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
+	vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
+end
+
+-- Loop through the servers and set them up
+local servers = {
+	"html",
+	"cssls",
+	"rust_analyzer",
+	"ts_ls",
+	"jsonls",
+	"docker_compose_language_service",
+	"dockerls",
+	"pyright",
+	"tailwindcss",
+	"yamlls",
+}
+
+for _, server in ipairs(servers) do
+	lspconfig[server].setup({
+		on_attach = on_attach,
+		capabilities = require("cmp_nvim_lsp").default_capabilities(),
+	})
+end
+
+-- Special configuration for lua_ls
+lspconfig.lua_ls.setup({
+	on_attach = on_attach,
+	capabilities = require("cmp_nvim_lsp").default_capabilities(),
 	settings = {
 		Lua = {
 			diagnostics = {
@@ -29,23 +85,9 @@ require("lspconfig").lua_ls.setup({
 	},
 })
 
-require("lspconfig").solargraph.setup({})
-require("lspconfig").tsserver.setup({})
-
-vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-	callback = function(ev)
-		-- Enable completion triggered by <c-x><c-o>
-		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-
-		-- Buffer local mappings.
-		-- See `:help vim.lsp.*` for documentation on any of the below functions
-		local opts = { buffer = ev.buf }
-		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-		vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-		vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, {})
-	end,
-})
+-- Optional: Configure diagnostic signs
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
