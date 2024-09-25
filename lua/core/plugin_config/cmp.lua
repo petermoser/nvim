77c515b8c768ee929c4ab98bf81691afa260fc2a
petermoser <peter.moser@gmail.com>
2024-09-25 17:36:51 +0200

    added some options to auto completion

--- lua/core/plugin_config/cmp.lua
+++ lua/core/plugin_config/cmp.lua
@@ -1,3 +1,4 @@
+-- Configures the auto completion-nvim plugin
 local cmp = require("cmp")
 
 cmp.setup({
@@ -6,12 +7,18 @@ cmp.setup({
 			require("luasnip").lsp_expand(args.body)
 		end,
 	},
+	window = {
+		completion = cmp.config.window.bordered(), -- Optional: Add borders to the completion window
+		documentation = cmp.config.window.bordered(), -- Show documentation window with borders
+	},
 	mapping = cmp.mapping.preset.insert({
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-e>"] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<C-b>"] = cmp.mapping.scroll_docs(-4), -- Scroll docs up
+		["<C-f>"] = cmp.mapping.scroll_docs(4), -- Scroll docs down
+		["<C-Space>"] = cmp.mapping.complete(), -- Trigger completion
+		["<C-e>"] = cmp.mapping.abort(), -- Abort completion
+		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Confirm selection
+		["<C-n>"] = cmp.mapping.select_next_item(), -- Move to next item
+		["<C-p>"] = cmp.mapping.select_prev_item(), -- Move to previous item
 	}),
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
