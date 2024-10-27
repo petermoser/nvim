769bf2a60d7f3a00597e47f1b4089edfbb4a58fc
petermoser <peter.moser@gmail.com>
2024-10-27 12:07:34 +0100

    Minor changes

--- lua/core/plugin_config/treesitter.lua
+++ lua/core/plugin_config/treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup({
-	-- A list of parser names, or "all"
+	-- List of parser names or "all"
 	ensure_installed = {
 		"c",
 		"lua",
@@ -18,7 +18,35 @@ require("nvim-treesitter.configs").setup({
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
 	auto_install = true,
+
+	-- Enable syntax highlighting
 	highlight = {
 		enable = true,
+	},
+
+	-- Additional modules
+	refactor = {
+		highlight_definitions = true, -- Highlight definitions within a scope
+		highlight_current_scope = { enable = true }, -- Highlight the current scope
+
+		-- Smart rename functionality
+		smart_rename = {
+			enable = true,
+			keymaps = {
+				smart_rename = "gR", -- Keymap to rename the variable under cursor
+			},
+		},
+
+		-- Navigation: Go to definition, references, etc.
+		navigation = {
+			enable = true,
+			keymaps = {
+				goto_definition = "gd", -- Keymap to go to definition
+				list_definitions = "gD", -- List all definitions in the file
+				list_definitions_toc = "gO", -- Table of contents for definitions
+				goto_next_usage = "<A-*>", -- Go to next usage
+				goto_previous_usage = "<A-#>", -- Go to previous usage
+			},
+		},
 	},
 })
