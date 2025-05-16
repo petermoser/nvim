68f17e4b33bdfe4d47ecfbe71d9e75033bf5fad4
petermoser <peter.moser@gmail.com>
2025-05-16 13:25:44 +0200

    fix: updated dadbod

--- lua/core/plugin_config/dadbod.lua
+++ lua/core/plugin_config/dadbod.lua
@@ -1,7 +1,7 @@
 vim.g.dbs = {
 	spotlight1 = "postgres://client_rw_app_user:"
 		.. os.getenv("SPOTLIGHT1_DB_PASSWORD")
-		.. "@ec2-18-206-21-146.compute-1.amazonaws.com:6432/spotlight1",
+		.. "@ec2-3-218-111-210.compute-1.amazonaws.com:6432/spotlight1",
 	spotlight2 = "postgres://client_rw_app_user:"
 		.. os.getenv("SPOTLIGHT2_DB_PASSWORD")
 		.. "@ec2-44-218-180-184.compute-1.amazonaws.com:6432/spotlight2",
